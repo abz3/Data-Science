3c43462a8ac056bf3440036139937aa7390efafb
Alex Zhong <71532358+abz3@users.noreply.github.com>
2021-01-14 17:16:36 -0600

    Add files via upload

---
+++ Evictions.py
@@ -0,0 +1,108 @@
+# Just imports the neccesary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Fetches data; souce: https://evictionlab.org/eviction-tracking/get-the-data/
+# IMPORTANT: Baseline data for this is 2015-2016
+evictiondata = pd.read_csv('Houston_evictions.csv')
+
+# Gives quick preview
+evictiondata.head(3)
+evictiondata.shape
+
+
+def total(evictiondata):
+    """
+    Computes the total evictions filed
+    :param evictiondata: the csv file of evictions filed
+    :return: An int repersenting the total
+    """
+    total = 0
+    for index, row in evictiondata.iterrows():
+        total += row['filings_2020']
+
+
+def weekly(evictiondata):
+    """
+    Eviction filings broken down into a week-by-week basis
+    :param evictiondata: the csv file of evictions filed
+    :return: a dictionary with the keys being the week, and the values being the evictions filed within that week
+    """
+    evictions_per_week = {}
+    for index, row in evictiondata.iterrows():
+        if row['week_date'] not in evictions_per_week.keys():
+            evictions_per_week[row['week_date']] = row['filings_2020']
+        else:
+            evictions_per_week[row['week_date']] += row['filings_2020']
+    return evictions_per_week
+
+
+def graphify(evictions_per_week):
+    """
+    Visualizes the week by week eviction data into a graph
+    :param evictions_per_week: dictionary with the keys being the week, and the values being the evictions filed within that week
+    :return: displays a graph: x values is time by week, y value is evictions filed
+    """
+    weeks = []
+    for week in evictions_per_week.keys():
+        if '2020' in week:
+            weeks.append(week)
+    evictions_filed = []
+    for week in weeks:
+        evictions_filed.append(evictions_per_week[week])
+    plt.figure(figsize=(50, 10))
+    plt.plot(weeks, evictions_filed)
+    plt.xlabel('Date')
+    plt.ylabel('Evictions filed')
+    plt.title('Evictions filed by the week')
+    plt.show()
+
+
+def graph_baseline(evictiondata):
+    """
+    Graphs the baseline eviction data of 2015-2016 in the same format
+    :param evictiondata: csv file of evictions filed
+    :return: displays a graph: x values being time by week, y values being evictioned filed in 2015-2016
+    """
+    base_evictions_per_week = {}
+    for index, row in evictiondata.iterrows():
+        if row['week_date'] not in base_evictions_per_week.keys():
+            base_evictions_per_week[row['week_date']] = row['filings_avg']
+        elif row['GEOID'] != 'sealed':
+            base_evictions_per_week[row['week_date']] += row['filings_avg']
+    base_evictions_filed = []
+    for week in weeks:
+        base_evictions_filed.append(base_evictions_per_week[week])
+
+    plt.figure(figsize=(50, 10))
+    plt.plot(weeks, base_evictions_filed, color='orange')
+    plt.title('Base Evictions filed by the week')
+    plt.xlabel('Date')
+    plt.ylabel('Evictions filed')
+    plt.show()
+
+
+def cross_analyze(evictiondata):
+    """
+    Cross analyzes the baseline with 2020's eviction data. NOTE* Requires you to run the above functions
+    :param evictiondata: csv file of eviction data
+    :return: a compiled graph that also notes when the Texas supreme court put a temporary stay on evictions
+    """
+    plt.figure(figsize=(50, 10))
+    plt.plot(weeks, evictions_filed, label='2020')
+    plt.plot(weeks, base_evictions_filed, label='2015-2016')
+    plt.xlabel('Date')
+    plt.ylabel('Evictions filed')
+    plt.title('Evictions filed by the week')
+    plt.legend()
+    plt.annotate('Texas Supreme Court puta a temporary stay on eviction proceedings.', xy=('3/8/2020', 1511))
+    plt.show()
+
+
+# Implementation
+print(total(evictiosndata))
+evictions_per_week = weekly(evictiondata)
+graphify(evictions_per_week)
+graph_baseline(evictiondata)
+cross_analyze(evictiondata)
